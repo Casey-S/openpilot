379fa1a97830ff0e9e420631bcb9eee65c46356d
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-02-13 09:36:35 +0100

    Update pid_lat.py

--- selfdrive/controls/lib/pid_lat.py
+++ selfdrive/controls/lib/pid_lat.py
@@ -13,9 +13,9 @@ def apply_deadzone(error, deadzone):
 
 class PIController():
   def __init__(self, k_p, k_i, k_f=1., pos_limit=None, neg_limit=None, rate=100, sat_limit=0.8, convert=None):
-    self._k_p = self.op_params.get('lat_kp')
-    self._k_i = self.op_params.get('lat_ki')
-    self.k_f = self.op_params.get('lat_kf')
+    self.k_p = self.op_params.get('long_kp')
+    self.k_i = self.op_params.get('long_ki')
+    self.k_f = self.op_params.get('long_kf')
 
     self.pos_limit = pos_limit
     self.neg_limit = neg_limit
@@ -49,9 +49,13 @@ class PIController():
     self.saturated = False
     self.control = 0
 
+  def update_tuning(self):
+    self.k_p = self.op_params.get('long_kp')
+    self.k_i = self.op_params.get('long_ki')
+    self.k_f = self.op_params.get('long_kf')
+
   def update(self, setpoint, measurement, speed=0.0, check_saturation=True, override=False, feedforward=0., deadzone=0., freeze_integrator=False):
-    self.k_p = self.op_params.get('lat_P', default=0.3)  # gets the prop gain every iteration (max once every 5 seconds, change in op_params.py)
-    self.k_i = self.op_params.get('lat_I', default=0.05)
+    self.update_tuning()
     self.speed = speed
 
     error = float(apply_deadzone(setpoint - measurement, deadzone))
