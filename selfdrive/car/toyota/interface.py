09866007829e4ec0772eb441843c082ea6821f3d
Casey-S <caseyspen@gmail.com>
2020-03-22 22:07:09 -0700

    Set canValid in toyota/interfaces.py line 307 to False in attempt to see verbose CAN errors

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -304,7 +304,7 @@ class CarInterface(CarInterfaceBase):
     # create message
     ret = car.CarState.new_message()
 
-    ret.canValid = True #self.cp.can_valid and self.cp_cam.can_valid
+    ret.canValid = False #self.cp.can_valid and self.cp_cam.can_valid
 
     # speeds
     ret.vEgo = self.CS.v_ego
